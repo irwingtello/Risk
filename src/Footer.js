7af57aee87a7fe0f84df4a0b0494f755455389ce
Irwing Tello <28800239+irwingtello@users.noreply.github.com>
2023-01-11 00:26:35 -0600

    New footer message

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,7 @@ function Footer() {
     <div>
       <hr />
       <p>
-        The Baro Score is intended to provide general information and should not
-        be relied upon as financial advice. Baro Score should not be used as a
-        substitute for professional financial advice and does not take into
-        account the individual financial circumstances of the user. The user
-        should not make any decisions based solely on the output from this score
-        generator. The user should always seek independent professional advice
-        prior to making any investment decisions. This disclaimer applies to all
-        services provided by this risk profile generator known as Baro Score.
+      This website is intended to provide general information only. The information provided on this website is not intended to constitute financial, legal, or other professional advice, and should not be relied upon as such. Please seek professional advice if you require specific advice on any financial, legal, or other matter. We do not accept any responsibility for any loss which may arise from reliance on information or materials published on this website.
       </p>
     </div>
   );
