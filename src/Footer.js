44dfd6e665ea1dc8292907b6f3776c1cbfcc0749
Dylan Molina <86082012+dylanintech@users.noreply.github.com>
2023-01-22 15:49:12 -0600

    cambio de risk.it a riskit para renombre

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,14 @@ function Footer() {
     <div>
       <hr />
       <p>
-        risk.it is intended to provide general information and should not
-        be relied upon as financial advice. risk.it should not be used as a
+        riskit is intended to provide general information and should not
+        be relied upon as financial advice. riskit should not be used as a
         substitute for professional financial advice and does not take into
         account the individual financial circumstances of the user. The user
         should not make any decisions based solely on the output from this score
         generator. The user should always seek independent professional advice
         prior to making any investment decisions. This disclaimer applies to all
-        services provided by this risk profile generator known as risk.it.
+        services provided by this risk profile generator known as riskit.
       </p>
     </div>
   );
